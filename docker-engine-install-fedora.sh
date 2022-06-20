4e95a65d6fb7ac5f49b03fb662638a790f0c0c2a
cbritos2016 <cristian@britos.net>
2022-06-20 12:20:01 -0400

    Update docker-engine-install-fedora.sh

--- docker-engine-install-fedora.sh
+++ docker-engine-install-fedora.sh
@@ -20,3 +20,35 @@ sudo dnf config-manager \
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 systemctl start docker
 sudo docker run hello-world
+
+#
+#
+#
+#
+
+sudo dnf -y install docker-compose
+
+#FILE: docker-compose.yml
+#
+#version: '2'
+#
+#services:
+#  urbackup:
+#    image: uroni/urbackup-server:latest
+#    container_name: urbackup
+#    restart: unless-stopped
+#    environment:
+#      - PUID=1000 # Enter the UID of the user who should own the files here
+#      - PGID=1000 # Enter the GID of the user who should own the files here
+#      - TZ=America/Chicago # Enter your timezone
+#    volumes:
+#      - /home/crisb/storage:/var/urbackup
+#      - /home/crisb/data:/backups
+#      # Uncomment the next line if you want to bind-mount the www-folder
+#      #- /path/to/wwwfolder:/usr/share/urbackup
+#    network_mode: "host"
+#    # Activate the following two lines for BTRFS support
+#    cap_add:
+#      - SYS_ADMIN
+
+sudo docker-compose up 
